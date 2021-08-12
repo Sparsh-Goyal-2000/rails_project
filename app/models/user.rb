8efb2178a9b08f6b368aab140bec8237913d2e0e
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-08-12 12:20:47 +0530

    callbacks commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   
   has_secure_password
 
+  after_create :notify_with_welcome_email, if: :email?
+  before_update :check_admin
+  before_destroy :check_admin
   after_destroy :ensure_an_admin_remains
 
   class Error < StandardError
@@ -19,5 +22,16 @@ class User < ApplicationRecord
     if User.count.zero?
       raise Error.new "Can't delete last User"
     end
+  end
+
+  def check_admin
+    if email.eql?('admin@depot.com')
+      errors.add(:email, 'can\'t delete or update admin')
+      throw :abort 
+    end
+  end
+
+  def notify_with_welcome_email
+    UserMailer.created(self).deliver
   end
 end
