38cb3f949d45c779af70d97faedb78d7e2dd5ec3
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-08-18 18:23:50 +0530

    query changes

--- app/views/catagories/_catagory.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! catagory, :id, :title, :created_at, :updated_at
-json.url catagory_url(catagory, format: :json)
--- db/migrate/20210816165025_practice_migration2.rb
+++
@@ -1,6 +0,0 @@
-class PracticeMigration2 < ActiveRecord::Migration[6.1]
-  def up
-    change_column_default :products, :price, 6
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
