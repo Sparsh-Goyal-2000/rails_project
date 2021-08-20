cef7ff18f79eb37389aaa930845f2b78fe13e96a
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-08-20 13:18:51 +0530

    Validation Requested Changes 4th

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,3 @@
-require_relative '../validators/url_validator'
-require_relative '../validators/compare_price_validator'
-
 class Product < ApplicationRecord
 
   VALID_PERMALINK_REGEX = /\A([[:alnum:]]+-){2,}([[:alnum:]]+)\z/
@@ -9,33 +6,38 @@ class Product < ApplicationRecord
   PERMALINK_ERROR_MESSAGE = 'should have minimum 3 words separated by hyphen'
   DESCRIPTION_ERROR_MESSAGE = 'should be between 5 to 10 words'
   LINE_ITEMS_PRESENT_MESSAGE = 'Line Items present'
-
-  validates :title, presence: true, uniqueness: true
-  validates :description, allow_blank: true, format: {
-    with: VALID_DESCRIPTION_REGEX,
-    message: DESCRIPTION_ERROR_MESSAGE
-  }
-  validates :image_url, allow_blank: true, url: true
-  validates :price, presence: true, numericality: { 
-    greater_than_or_equal_to: MINIMUM_PRICE,
-    only_integer: true
-  }
-
-  # Using Custom Validator
-   validates_with ComparePriceValidator
-
-  # Without using Custom Validator
-  validates :price, numericality: { 
-    greater_than: :discount_price
-  }, if: :discount_price
-
-  validates :permalink, uniqueness: true, allow_blank: true, format: {
-    with: VALID_PERMALINK_REGEX,
-    message: PERMALINK_ERROR_MESSAGE
-  }
-
+  
   has_many :line_items
   has_many :orders, through: :line_items
+
+  validates :title, :price, presence: true
+  with_options allow_blank: true do
+    validates :title, uniqueness: true
+    validates :description, format: {
+      with: VALID_DESCRIPTION_REGEX,
+      message: DESCRIPTION_ERROR_MESSAGE
+    }
+    validates :image_url, url: true
+    validates :permalink, uniqueness: true, format: {
+      with: VALID_PERMALINK_REGEX,
+      message: PERMALINK_ERROR_MESSAGE
+    }
+  end
+  
+  # Using Custom Validator
+  validates_with ComparePriceValidator
+
+  with_options allow_nil: true do
+    validates :price, numericality: { 
+      greater_than_or_equal_to: MINIMUM_PRICE,
+      only_integer: true
+    }
+
+    # Without using Custom Validator
+    validates :price, numericality: { 
+      greater_than: :discount_price
+    }, if: :discount_price
+  end
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
