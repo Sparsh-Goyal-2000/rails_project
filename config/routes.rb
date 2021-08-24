78e3faa31ee528d9c479cabbc381f3119ad892cf
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-08-24 17:22:29 +0530

    Routing Changes 1st

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,35 @@
 Rails.application.routes.draw do
-  get 'catagories/catagory_with_subcatagory', to: 'catagories#catagory_with_subcatagory'
-
-  namespace :admin do
-    get :index
-    get :reports
-    get :catagories
-  end
-
-  namespace :users do
-    get :orders
-    get :line_items
-  end
-
-  controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    get 'logout' => :destroy
-  end
-
-  resources :support_requests, only: [ :index, :update ]
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  resources :catagories
   root 'store#index', as: 'store_index'
-  resources :products do
-    get :who_bought, on: :member
+
+  constraints(-> (req) { req.env["HTTP_USER_AGENT"] !~ /Firefox/ }) do
+    get 'catagories/catagory_with_subcatagory', to: 'catagories#catagory_with_subcatagory'
+    get 'my-orders', to: 'users#orders'
+    get 'my-items', to: 'users#line_items'
+    get 'catagories/:id/books', to: 'store#index', constraints: { id: /\D+/ }
+    namespace :admin do
+      get :index
+      get :reports
+      get :catagories
+    end
+    namespace :users do
+      get :orders
+      get :line_items
+    end
+    controller :sessions do
+      get 'login' => :new
+      post 'login' => :create
+      get 'logout' => :destroy
+    end
+    resources :catagories do
+      resources :products, path: :books
+    end
+    resources :support_requests, only: [ :index, :update ]
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products, path: :books do
+      get :who_bought, on: :member
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
